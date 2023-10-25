d88d62742f979948cc5e04c17ac1fa1ab7d79b55
francoisbradesi <francois.bradesi@etu.minesparis.psl.eu>
2023-10-25 14:54:13 +0200

    Mesure de temperature

--- hardwareProgramming/Riviere_2023/internals/Temp_Sensor.cpp
+++ hardwareProgramming/Riviere_2023/internals/Temp_Sensor.cpp
@@ -1,18 +1,23 @@
 // This file will contain all the code relative to the measurement of temperature
-
 // The data type of a temperature measurement
 #define TEMP_T unsigned short
 
 
 // Initialise the temperature sensor for the first time. 
-void InitialiseTempSensor(/* Parameters */) {
-  // Todo
+void InitialiseTempSensor(int alimPin, int measurePin) {
+  // Attribute a pin to the temperature measurement and the power
+  pinMode(alimPin, OUTPUT);
+  pinMode(measurePin, INPUT);
+  
 }
 
 // Measure the temperature
-TEMP_T MeasureTemperature(/* Parameters */) {
-  // Todo
+TEMP_T MeasureTemperature(int alimPin, int measurePin) {
+  //Power the sensor only when we measure
+  //Read the measured temperature on the defined pin
+  //Unpower the sensor
+  digitalWrite(alimPin, HIGH);
+  unsigned int temp = analogRead(measurePin);
+  digitalWrite(alimPin, LOW);
+  return temp;
 }
-
-
-// Other functions ...
