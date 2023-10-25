8078d50cba1c6d7dd7c3f30e18956f70f2181af4
zhanquen <147552860+zhanquen@users.noreply.github.com>
2023-10-25 09:34:47 +0200

    modifi charge hydraulique multip

--- pyheatmy2022/pyheatmy/utils.py
+++ pyheatmy2022/pyheatmy/utils.py
@@ -267,7 +267,7 @@ def compute_H(moinslog10K, Ss, all_dt, isdtconstant, dz, H_init, H_riv, H_aq, al
 
 @njit
 def compute_T_stratified(
-    moinslog10K_list, n_list, lambda_s_list, rhos_cs_list, all_dt, dz, H_res, H_riv, H_aq, T_init, T_riv, T_aq, alpha=ALPHA
+    moinslog10K_list, n_list, lambda_s_list, rhos_cs_list, all_dt, dz, H_res, H_riv, H_aq, nablaH, T_init, T_riv, T_aq, alpha=ALPHA
 ):
     """ Computes T(z, t) by solving the heat equation : dT/dt = ke Delta T + ae nabla H nabla T, for an heterogeneous column.
 
@@ -318,14 +318,14 @@ def compute_T_stratified(
 
     # First we need to compute the gradient of H(z, t)
 
-    nablaH = zeros((n_cell, n_times), float32)
+    # nablaH = zeros((n_cell, n_times), float32)
 
-    nablaH[0, :] = 2*(H_res[1, :] - H_riv)/(3*dz)
+    # nablaH[0, :] = 2*(H_res[1, :] - H_riv)/(3*dz)
 
-    for i in range(1, n_cell - 1):
-        nablaH[i, :] = (H_res[i+1, :] - H_res[i-1, :])/(2*dz)
+    # for i in range(1, n_cell - 1):
+    #     nablaH[i, :] = (H_res[i+1, :] - H_res[i-1, :])/(2*dz)
 
-    nablaH[n_cell - 1, :] = 2*(H_aq - H_res[n_cell - 2, :])/(3*dz)
+    # nablaH[n_cell - 1, :] = 2*(H_aq - H_res[n_cell - 2, :])/(3*dz)
 
     # Now we can compute T(z, t)
 
