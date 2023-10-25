fc1f21f7455854ddc74f8d6733b10b320357ae50
Matteo Leturcq--Daligaux <matteo.leturcq--daligaux@etu.minesparis.psl.eu>
2023-10-25 16:53:48 +0200

    Revert

---
+++ hardwareProgramming/Rive_2023/Low_Power.ino
@@ -0,0 +1,9 @@
+// This file is responsible of switching to low-power mode
+
+#include <ArduinoLowPower.h>
+
+
+// Switch to low-power mode for a given amount of time
+void Sleep(unsigned int millis) {
+  // Todo
+}
