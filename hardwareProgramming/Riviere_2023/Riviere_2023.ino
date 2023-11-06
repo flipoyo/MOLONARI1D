37d7b39a1770459ef6f892e5a05523f23265f219
francoisbradesi <francois.bradesi@etu.minesparis.psl.eu>
2023-11-06 15:17:47 +0100

    étalonnage capteur

--- hardwareProgramming/Riviere_2023/Riviere_2023.ino
+++ hardwareProgramming/Riviere_2023/Riviere_2023.ino
@@ -1,57 +1,34 @@
-/*
-This firmware will be is meant for the arduino in the river bed of the Molonari system.
-
-Functionalities :
-  - Making measurements from the temperature and pressure sensors.
-  - Sending the measurements via LoRa
-  - Saving the measurements in an internal memory
-
-Required hardware :
-Arduino MKR WAN 1310
-*/
-
-#include "internals/Lora.hpp"
-#include "internals/Low_Power.cpp"
-#include "internals/Pressure_Sensor.hpp"
-#include "internals/Temp_Sensor.cpp"
-#include "internals/Time.cpp"
 #include "internals/Internal_Log.cpp"
-#include "internals/Measure.h"
-// #include "internals/testSample.h" Deprecated
-// #include "internals/InternalData.h" Deprecated
+int sensePin1 = A1; //This is the Arduino Pin that will read the sensor output
+int sensePin2 = A2;
+int sensePin3 = A3;
+int sensePin4 = A4;
 
 
-PressureSensor pressureSensor(A6, 6);
-
+double ConvertTemp(int measure){
+  return ((double)measure*5/1024-0.5)*100;
+}
 
 void setup() {
   Serial.begin(9600);
-
-  InitialiseLora();
-  InitialiseRTC();
   bool didIt = InitialiseLog();
-
-  if (didIt) {
-    Serial.println("SD card initialized");
-
-    // TESTINGS
-
-    unsigned int test1[4] = {sizeof("Id,Date,Time,Capteur1,Capteur2,Capteur3,Capteur4"),sizeof("\n"),sizeof(Measure),777};
-    LogData(test1);
-
-    unsigned int test2[4] = {1,9,0,456456};
-    LogData(test2);
+  while (!Serial){
   }
-  else {
-    Serial.println("SD card failed to initialize");
-  }
-
 }
 
 void loop() {
-  // Get immidiately the data stored in FlashMemory
+    int sensorInput1 = analogRead(sensePin1); //read the analog sensor and store it
+    int sensorInput2 = analogRead(sensePin2); //read the analog sensor and store it
+    int sensorInput3 = analogRead(sensePin3); //read the analog sensor and store it
+    int sensorInput4 = analogRead(sensePin4); //read the analog sensor and store it
+    double temp1 = ConvertTemp(sensorInput1);
+    double temp2 = ConvertTemp(sensorInput2);
+    double temp3 = ConvertTemp(sensorInput3);
+    double temp4 = ConvertTemp(sensorInput4);
+    Serial.println(String(temp1) + "  " + String(temp2) + "  " +String(temp3) +"  " + String(temp4) );
+    
 
-  // PRESSURE_T pressure = pressureSensor.MeasurePressure();
-  // Serial.println(pressure);
-
+    double test1[4] = {temp1, temp2, temp3, temp4};
+    LogData(test1);
+    delay(1000);
 }
