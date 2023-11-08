5bda47c777e3ae26108210c1ef36b5a00c372c5f
francoisbradesi <francois.bradesi@etu.minesparis.psl.eu>
2023-11-08 09:54:45 +0100

    riviere pour test

--- hardwareProgramming/Riviere_2023/Riviere_2023.ino
+++ hardwareProgramming/Riviere_2023/Riviere_2023.ino
@@ -1,97 +1,27 @@
-<<<<<<< HEAD
-/*
-This firmware will be is meant for the arduino in the river bed of the Molonari system.
-
-Functionalities :
-  - Making measurements from the temperature and pressure sensors.
-  - Sending the measurements via LoRa
-  - Saving the measurements in an internal memory
-
-Required hardware :
-Arduino MKR WAN 1310
-*/
-
-#include "internals/Lora.cpp"
-#include "internals/Low_Power.cpp"
-#include "internals/Pressure_Sensor.hpp"
-#include "internals/Temp_Sensor.cpp"
-#include "internals/Time.cpp"
 #include "internals/Internal_Log_Initializer.cpp"
-#include "internals/Measure.h"
-#include "internals/Reader.h"
+#include "internals/Temp_Sensor.cpp"
 #include "internals/Writer.h"
-=======
-#include "internals/Internal_Log.cpp"
+
 int sensePin1 = A1; //This is the Arduino Pin that will read the sensor output
 int sensePin2 = A2;
 int sensePin3 = A3;
 int sensePin4 = A4;
->>>>>>> 2023bradesi
-
-const int CSpin = 6;
-
-<<<<<<< HEAD
-// Reader reader;
+const int CSPin = 6;
 Writer writer;
-PressureSensor pressureSensor(A6, 6);
 
-=======
-double ConvertTemp(int measure){
-  return ((double)measure*5/1024-0.5)*100;
+double ConvertTemp(int input){
+  return ((double)input*3.3/1024-0.5)*100;
 }
->>>>>>> 2023bradesi
-
 
 void setup() {
-  Serial.begin(9600);
-<<<<<<< HEAD
 
-  while (!Serial){
-  }
   
-  InitialiseLora();
-  InitialiseRTC();
-  bool connectionEstablished = InitialiseLog(CSpin);
+  bool connected = InitialiseLog(CSPin);
 
-  if (connectionEstablished) {
-    Serial.println("SD card initialized");
-    // reader.EstablishConnection();
+  if (connected) {
     writer.EstablishConnection();
-    // TESTINGS
-
-    
-    Serial.println("Start Writing...");
-
-
-    for (int i=0; i<5; i++){
-
-      unsigned int test[4] = {41,55455,123,1};
-      writer.LogData(test);
-
-      unsigned int test1[4] = {7,3,0,2};
-      writer.LogData(test1);
-    }
-
-    
-    Serial.println("Finished Writing");
-
-
-    }
-
-
-    else {
-      Serial.println("SD card failed to initialize");
-    }
-
-}
-
-void loop() {
-  // PRESSURE_T pressure = pressureSensor.MeasurePressure();
-  // Serial.println(pressure);
-=======
-  bool didIt = InitialiseLog();
-  while (!Serial){
   }
+
 }
 
 void loop() {
@@ -103,11 +33,9 @@ void loop() {
     double temp2 = ConvertTemp(sensorInput2);
     double temp3 = ConvertTemp(sensorInput3);
     double temp4 = ConvertTemp(sensorInput4);
-    Serial.println(String(temp1) + "  " + String(temp2) + "  " +String(temp3) +"  " + String(temp4) );
     
-
+    Serial.println(String(temp1) + "   " + String(temp2) + "   " + String(temp3) + "   " + String(temp4) + "   ");
     double test1[4] = {temp1, temp2, temp3, temp4};
-    LogData(test1);
-    delay(1000);
->>>>>>> 2023bradesi
+    writer.LogData(test1);
+    delay(15000);
 }
