bc0bf039523b9ebf6fd53ff12e0246d12224a268
Léopold Gravier <le78ld@gmail.com>
2023-11-13 11:15:59 +0100

    Mesures conso

---
+++ hardwareProgramming/Mesures conso/Conso_Sleep/Conso_Sleep.ino
@@ -0,0 +1,11 @@
+
+#include <ArduinoLowPower.h>
+
+void setup() {
+  // put your setup code here, to run once:
+  LowPower.deepSleep();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+}
