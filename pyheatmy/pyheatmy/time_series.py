cfdf9c401553f9e55134b6bca3a18547160241a6
nflipo <nicolas.flipo@minesparis.psl.eu>
2024-10-10 16:09:22 +0200

    demo_genData.ipynb works for python3.11.4, tested in an environment where molonaviz also works

--- pyheatmy/pyheatmy/time_series.py
+++ pyheatmy/pyheatmy/time_series.py
@@ -50,26 +50,31 @@ class Time_series:  # on simule un tableau de mesures
         self._depth_sensors = np.array(depth_sensors)
         self._real_z = np.array([0] + depth_sensors) + offset
 
-        self._dates = np.array([None])
-        self._time_array = np.array([None])
-        # le tableau d'observation des charges utilisable dans colonne
-        self._dH = np.array([None])
-        self._dH_perturb = np.array([None])  # avec perturbation
-        # récupère la liste de température observée de la rivière (au cours du temps)
-        self._T_riv = np.array([None])
-        self._T_riv_perturb = np.array([None])  # avec perturbation
-        # le tableau d'observation de la pression et de la température rivière
-        self._molonariP_data = None
-        self._molonariP_data = None
-        # récupère la liste de température observée de l'aquifère (au cours du temps)
-        self._T_aq = np.array([None])
-        self._T_aq_perturb = np.array([None])  # avec perturbation
-        # le tableau d'observation des températures utilisable dans colonne
-        self._T_Shaft = np.array([None])
-        self._T_Shaft_perturb = np.array([None])  # avec perturbation
+        self._generate_all_series()
 
-        self._T_Shaft_measures = None
-        self._molonariT_data = None  # avec perturbation
+
+        # self._dates = np.array([None])
+        # self._time_array = np.array([None])
+        # # le tableau d'observation des charges utilisable dans colonne
+        # self._dH = np.array([None])
+        # self._dH_perturb = np.array([None])  # avec perturbation
+        # # récupère la liste de température observée de la rivière (au cours du temps)
+        # self._T_riv = np.array([None])
+        # self._T_riv_perturb = np.array([None])  # avec perturbation     
+        # # récupère la liste de température observée de l'aquifère (au cours du temps)
+        # self._T_aq = np.array([None])
+        # self._T_aq_perturb = np.array([None])  # avec perturbation
+        # # le tableau d'observation des températures utilisable dans colonne
+        # self._T_Shaft = np.array([None])
+        # self._T_Shaft_perturb = np.array([None])  # avec perturbation
+
+        # self._T_Shaft_measures = None
+        # self._molonariT_data = None  # avec perturbation
+
+        # # le tableau d'observation de la pression et de la température rivière
+        # self._molonariP_data = None
+        # self._molonariP_data = None
+
 
     @classmethod
     def from_dict(cls, time_series_dict):
@@ -98,71 +103,43 @@ class Time_series:  # on simule un tableau de mesures
             self._dates = np.array(times_vir1)
             self._time_array = np.array(times_list)
 
-        # self._dates = pd.to_datetime(self._dates)
-        # print(f"initial dates : \n\t{self._dates}\n and time array : \n\t{self._time_array}")
-        # data = self._dates
-        # transformed_data = [(pd.Timestamp(dt), values) for dt, values in data]
-        # self._dates = transformed_data
-        # print(f"after conversion to timestamps dates : \n\t{self._dates}\n and time array : \n\t{self._time_array}")
-
     def _generate_dH_series(self,verbose=True):
-        # if self._dates.any() == None:
-        #     self._generate_dates_series()
         ts = create_periodic_signal(self._dates,self._param_dates[2],self._param_dH,"Hydraulic head differential",verbose=verbose)
-        # self._dH = convert_list_to_timestamp(ts,self)
         self._dH = ts
 
     def _generate_Temp_riv_series(self,verbose=True):  # renvoie un signal sinusoïdal de temperature rivière
-        # if self._dates.any() == None:
-        #     self._generate_dates_series()
         ts = create_periodic_signal(self._dates,self._param_dates[2],self._param_T_riv,"T_riv",verbose=verbose)
-        # self._T_riv = convert_list_to_timestamp(ts,self)
         self._T_riv = ts
 
     def _generate_Temp_aq_series(self,verbose=True):  # renvoie un signal sinusoïdal de temperature aquifère
-        # if self._dates.any() == None:
-        #     self._generate_dates_series()
         ts = create_periodic_signal(self._dates,self._param_dates[2],self._param_T_aq,"T_aq",verbose=verbose)
-        # self._T_aq = [(pd.Timestamp(dt), values) for dt, values in ts]
-        # self._T_aq = convert_list_to_timestamp(ts,self)
         self._T_aq = ts
 
     def _generate_Shaft_Temp_series(self,verbose=True):  # en argument n_sens_vir le nb de capteur (2 aux frontières et 3 inutiles à 0)
-        # initialisation
+        # The initialisation of the sensors of the shaft is made by last sensor = aquifer temperature, the intermediaite ones are linearly interpolated function of their distance to the river bottom between Triv and Taq
         n_sens_vir = len(self._depth_sensors)
         if verbose:
             print(f"Generating Shaft with {n_sens_vir} sensors")
-        if self._T_Shaft.any() == None:
-            self._T_Shaft = np.ones((len(self._dates), n_sens_vir)) * CODE_Temp  # le tableau qui accueille des données de températures de forçage
-            # self._T_Shaft[:, n_sens_vir - 1] = self._T_aq
-
-            # Loop through each date and perform interpolation
-            for i, date in enumerate(self._dates):
-                f = interp1d(
-                    [self._real_z[0], self._real_z[-1]], [self._T_riv[i], self._T_aq[i]]
-                )
-                self._T_Shaft[i, :] = f(self._depth_sensors)
-        
-            # Set the last column to _T_aq
-            self._T_Shaft[:, n_sens_vir - 1] = self._T_aq
-        #     self._T_Shaft[0] = f(self._depth_sensors)
-        # # T_init*np.ones(n_sens_vir-1) # pour les conditions initiales au niveau des capteurs T1, T2, T3, ... avant Taq
-        # # si T_shaft existe déjà (mais si maj) alors on maj seulement T_shaft_measure
-
+        self._T_Shaft = np.ones((len(self._dates), n_sens_vir)) * CODE_Temp  # le tableau qui accueille des données de températures de forçage
+        for i, date in enumerate(self._dates):
+            f = interp1d(
+                [self._real_z[0], self._real_z[-1]], [self._T_riv[i], self._T_aq[i]]
+            )
+            self._T_Shaft[i, :] = f(self._depth_sensors)
+    
+        # Set the last column to _T_aq
+        self._T_Shaft[:, n_sens_vir - 1] = self._T_aq
         if verbose:
             print(f"{n_sens_vir} sensors in the shaft")
             for i in range(n_sens_vir):
                  print(f"Temperature of Sensor {i} : {self._T_Shaft[:,i]}")
-        # self._T_Shaft_measures = list(zip(convert_to_timestamp(self._dates,self), self._T_Shaft))
         self._T_Shaft_measures = list(zip(self._dates, self._T_Shaft))
-
-
 
     def _perturbate(self,ts, sigma):#perturbates the time series with a normal distrib of sigma
         n_t=len(ts)
         new_ts=ts
         if sigma != None:
-            new_ts=ts+normal(0,sigma,n_t)
+            new_ts+=normal(0,sigma,n_t)
         return new_ts
     
     @checker
@@ -185,16 +162,11 @@ class Time_series:  # on simule un tableau de mesures
 
         # self._molonariP_data = list(zip(convert_to_timestamp(self._dates,self), list(zip(self._dH_perturb, self._T_riv_perturb)))) #emulates what comes from a pressure sensor
     def _generate_all_series(self,verbose=True):
-        if self._dates.any() == None:
-            self._generate_dates_series()
-        if self._dH.any() == None:
-            self._generate_dH_series(verbose=verbose)
-        if self._T_riv.any() == None:
-            self._generate_Temp_riv_series(verbose=verbose)
-        if self._T_aq.any() == None:
-            self._generate_Temp_aq_series(verbose=verbose)
-        if self._T_Shaft.any() == None:
-            self._generate_Shaft_Temp_series(verbose=verbose)   
+        self._generate_dates_series()
+        self._generate_dH_series(verbose=verbose)
+        self._generate_Temp_riv_series(verbose=verbose)
+        self._generate_Temp_aq_series(verbose=verbose)
+        self._generate_Shaft_Temp_series(verbose=verbose)   
 
         #now generating perturbated measurments and emulating what comes from molonari devices
         self._generate_perturb_T_riv_dH_series()
@@ -240,26 +212,25 @@ class Time_series:  # on simule un tableau de mesures
         plt.show()
 
 
-    def _measures_column_one_layer(self, column, layer_list, nb_cell,verbose=True):
+    def _measures_column_one_layer(self, column, layer_list, nb_cell,verbose=False):
         column.compute_solve_transi(layer_list, nb_cell,verbose=verbose)
         self._set_Shaft_Temp_series(column.get_temperature_at_sensors(verbose=verbose),column.get_id_sensors(),verbose=verbose)        
         self._generate_perturb_Shaft_Temp_series()
 
-    @ _generate_perturb_Shaft_Temp_series.needed
-    def _print_molonariDevice(self, column, type,verbose=True):
-
-
-        if type == "molonariT":
-            if verbose:
-                print("MolonariT data")
-                print(self._molonariT_data)
-            self._plot_molonariT_data()
-        elif type == "molonariP":
-            if verbose:
-                print("MolonariP data")
-                print(self._molonariP_data)
-        else:
-            print("Type not recognized")
+# # method not finished, can be erased
+#     @ _generate_perturb_Shaft_Temp_series.needed
+#     def _print_molonariDevice(self, column, type,verbose=True):
+#         if type == "molonariT":
+#             if verbose:
+#                 print("MolonariT data")
+#                 print(self._molonariT_data)
+#             self._plot_molonariT_data()
+#         elif type == "molonariP":
+#             if verbose:
+#                 print("MolonariP data")
+#                 print(self._molonariP_data)
+#         else:
+#             print("Type not recognized")
 
 
 
