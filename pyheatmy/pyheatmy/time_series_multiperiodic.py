0d90e2630c279dfd8c00928fb1ad9cd5107627db
alexandrenoel20 <alexandre.noel@etu.minesparis.psl.eu>
2024-10-21 10:07:40 +0200

    creation of class, code of values_time_series to associate list of tuple (t,T) to the object if type ts in the init

---
+++ pyheatmy/pyheatmy/time_series_multiperiodic.py
@@ -0,0 +1,18 @@
+import numpy as np 
+
+class time_series_multiperiodic : 
+    def __init__(self, type):
+        assert type in ['ts', 'mutli_periodic'], 'type must be either ts or multi_periodic'
+        self.type = type
+    
+    if type == 'ts' :
+        def values_time_series(self, t, T):
+            assert len(t) == len(T), 't and T must have the same length'
+            L=np.zeros(len(t),2)
+            for i in range(len(t)):
+                L[i,0]=t[i]
+                L[i,1]=T[i]
+            self.time_series = L
+
+    if type == 'multi_periodic' :
+        def create_mutiperiodic_signal()
