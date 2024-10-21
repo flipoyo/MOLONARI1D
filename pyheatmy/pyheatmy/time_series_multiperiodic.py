37ef15dac06fe17a4062b61f2126603f3176f3d7
Colin Drouineau <colin.drouineau@etu.minesparis.psl.eu>
2024-10-21 11:27:00 +0200

    working on class time_series_multiperiodic  !! not tried !!

--- pyheatmy/pyheatmy/time_series_multiperiodic.py
+++ pyheatmy/pyheatmy/time_series_multiperiodic.py
@@ -1,4 +1,14 @@
 import numpy as np 
+import matplotlib.pyplot as plt
+
+# à mettre dans utils (?)
+def two_column_array(a1, a2):
+    assert len(a1) == len(a2), 't and T must have the same length'
+    L=np.zeros(len(a1),2)
+    for i in range(len(t)):
+        L[i,0]=a1[i]
+        L[i,1]=a2[i]
+    return L
 
 class time_series_multiperiodic : 
     def __init__(self, type):
@@ -7,15 +17,29 @@ class time_series_multiperiodic :
     
     if type == 'ts' :
         def values_time_series(self, t, T):
-            assert len(t) == len(T), 't and T must have the same length'
-            L=np.zeros(len(t),2)
-            for i in range(len(t)):
-                L[i,0]=t[i]
-                L[i,1]=T[i]
-            self.time_series = L
+            self.time_series = two_column_array(t, T)
 
     if type == 'multi_periodic' :
-        def create_mutiperiodic_signal(self, offset=12, duration, step, amplitude, period):
+        def create_mutiperiodic_signal(self, amplitude, period, number_of_points, offset=12):
             assert len(amplitude)==len(period), 'amplitude and period must have the same length'
-            assert duration%step==0, 'duration must be a multiple of step'
+            p_max = max(period)
+            step = p_max / number_of_points
+            t = np.array([i*step for i in range(number_of_points)])
+            T = np.zeros(number_of_points)
+            T += offset
+            for i in range(len(amplitude)):
+                T += amplitude[i] * np.sin(2*np.pi/period[i]*t)
+            self.multi_periodic = two_column_array(t, T)
+    
+    def plot(a, time_unit = 's'):
+        assert a.ndim == 2 and len(a[0]) == len(a[1]), 'a must be a n * 2 array'
+        assert type(time_unit) == str, 'time_unit musty be of type str'
+        plt.plot(a[0], a[1])
+        plt.title("Temperature profile")
+        plt.xlabel("time" + str(time_unit))
+        plt.ylabel("temperature °C")
+    
+            
+            
+
             
