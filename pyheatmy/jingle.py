3e64a8c1fbe3edbd816d497e39bf24e287f84c88
Mathieu Jousson <mathieu.jousson@gmail.com>
2025-10-17 15:33:28 +0200

    ...

--- pyheatmy/jingle.py
+++ pyheatmy/jingle.py
@@ -1,11 +1,9 @@
 from IPython.display import Audio, display
 import numpy as np
 
-audio_file = r'.\Frequentieeeeel.mp3'
+def musique():
 
-try:
+    audio_file = r'.\Frequentieeeeel.mp3'
+
     # Créer un élément audio qui se déclenche automatiquement
     display(Audio(url=audio_file, autoplay=True))
-    
-except FileNotFoundError:
-    print(f"Attention : Le fichier audio '{audio_file}' n'a pas été trouvé.")
