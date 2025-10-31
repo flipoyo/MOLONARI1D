628316f35b8caf6d9218504e333e5dca5b468e3d
EulalieContreras <eulalie.contreras13@gmail.com>
2025-10-31 16:56:06 +0100

    mise a jour git ignore et suite correction main_relay

--- hardware/field_code/libs/LoRaWan_Molonari/LoRaWan_Molonari.hpp
+++ hardware/field_code/libs/LoRaWan_Molonari/LoRaWan_Molonari.hpp
@@ -4,12 +4,14 @@
 #include <Arduino.h>
 #include <MKRWAN.h>
 #include <queue>
+#include <SD.h>
 
 class LoraWANCommunication {
 public:
     LoraWANCommunication();
     bool begin(const String& appEui, const String& appKey);
     bool sendQueue(std::queue<String>& sendingQueue);
+    bool receiveConfig(const char* configFilePath, bool modif);
 
 private:
     LoRaModem modem;
