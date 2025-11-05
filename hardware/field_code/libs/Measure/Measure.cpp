2134d98e954e2d8ad6681ab1af0802b2e42aa2fd
EulalieContreras <eulalie.contreras13@gmail.com>
2025-11-05 12:17:16 +0100

    delay plus court

--- hardware/field_code/libs/Measure/Measure.cpp
+++ hardware/field_code/libs/Measure/Measure.cpp
@@ -27,7 +27,7 @@ Sensor::Sensor(int _dataPin, int _enablePin, String _type_capteur)
 
 double Sensor::get_voltage() { 
   digitalWrite(enablePin, HIGH);
-  delay(5000);
+  delay(1000);
   double voltage = analogRead(dataPin);
   DEBUG_LOG("measured voltage of Pin " + String(dataPin) + " of " + String (voltage));
   digitalWrite(enablePin, LOW);
