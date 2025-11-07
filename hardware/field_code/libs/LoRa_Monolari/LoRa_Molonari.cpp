62d6183046d7ddb44743ce9d0d1ce6543b9fc3c7
EulalieContreras <eulalie.contreras13@gmail.com>
2025-11-07 13:01:16 +0100

    euh test debug toujours sur les memes fonctions

--- hardware/field_code/libs/LoRa_Monolari/LoRa_Molonari.cpp
+++ hardware/field_code/libs/LoRa_Monolari/LoRa_Molonari.cpp
@@ -204,9 +204,9 @@ bool LoraCommunication::handshake(uint8_t &shift) {
                 DEBUG_LOG("MASTER: Received SYN-ACK, sending ACK");
                 digitalWrite(LED_BUILTIN, LOW);
                 delay(50);
-                for (int i = 0; i<40; i++){
+                for (int i = 0; i<10; i++){
                     digitalWrite(LED_BUILTIN, HIGH);
-                    sendPacket(packetNumber, ACK, "ACK");
+                    sendPacket(packetNumber, ACK, "SYN-ACK-ACK");
                     digitalWrite(LED_BUILTIN, LOW);
                     delay(100);
                 }
@@ -248,7 +248,7 @@ bool LoraCommunication::handshake(uint8_t &shift) {
             delay(500);// * (retries + 1));
             sendPacket(shift, SYN, "SYN-ACK");
             DEBUG_LOG("SLAVE: SYN-ACK sent");
-            if (receivePacket(packetNumber, requestType, payload) && requestType == ACK && payload == "ACK") {
+            if (receivePacket(packetNumber, requestType, payload) && requestType == ACK && payload == "SYN-ACK-ACK") {
                 DEBUG_LOG("SYN ACK WORKED - handshake done");
                 return true;
             }
